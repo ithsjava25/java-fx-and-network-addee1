46aeeb7e42b28f779fea61288cf3558e8adfb46a
Adam Ottosson <addeecsgo@gmail.com>
2025-11-13 23:57:29 +0100

    Adds a nested Attachment record

--- src/main/java/com/example/NtfyMessageDto.java
+++ src/main/java/com/example/NtfyMessageDto.java
@@ -3,4 +3,22 @@ package com.example;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record NtfyMessageDto(String id, long time, String event, String topic, String message) {}
+public record NtfyMessageDto(
+        String id,
+        long time,
+        long expires,
+        String event,
+        String topic,
+        String message,
+        Attachment attachment
+) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Attachment(
+            String name,
+            String type,
+            String url,
+            long expires,
+            long size
+    ){}
+
+}
